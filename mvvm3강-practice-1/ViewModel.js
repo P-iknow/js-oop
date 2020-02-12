960a6b3bbfd631f3ad8859fbb62b09376085db3e
P-iknow <apricotsoul@gmail.com>
2020-02-12 19:25:28 +0900

    practice: 3강 ViewModel  일부  완성

--- mvvm3강-practice-1/ViewModel.js
+++ mvvm3강-practice-1/ViewModel.js
@@ -14,3 +14,68 @@ const ViewmodelValue = class {
     Object.freze(this);
   }
 };
+
+const ViewModel = class extends ViewModelListener {
+  static get(data) {
+    return new ViewModel(data);
+  }
+  static #subjects = new Set();
+  static #inited = false;
+  static notify(vm) {
+    this.#subjects.add(vm);
+    if (this.#inited) return;
+    this.#inited = true;
+    const f = _ => {
+      this.#subjects.forEach(vm => {
+        if (vm.#isUpdated.size) {
+          vm.notify();
+          vm.#isUpdated.clear();
+        }
+      });
+      requestAnimationFrame(f);
+    };
+    requestAnimationFrame(f);
+  }
+  static define(vm, cat, obj) {
+    return Object.defineProperties(
+      obj,
+      Object.entries(obj).reduce((r, [k, v]) => {
+        r[k] = {
+          enumerable: true,
+          get: _ => v,
+          set: newV => {
+            v = newV;
+            vm.#isUpdated.add(new ViewmodelValue(vm.subkey, cat, k, v));
+          },
+        };
+        return r;
+      }, {})
+    );
+  }
+  style = {};
+  attributes = {};
+  properties = {};
+  events = {};
+  subkey = '';
+  parent = null;
+  #isUpdated = new Set();
+  #listeners = new Set();
+  constructor(data, _ = type(data, 'object')) {
+    super();
+    Object.entries(data).forEach(([k, v]) => {
+      if ('styles,attributes,properties'.includes(k)) {
+        if (!v || typeof v != 'object') throw `invalid object k:${k}, v:#{v}`;
+        this[k] = ViewModel.define(this, k, v);
+      } else {
+        Object.defineProperty(this, k, {
+          enumerable: true,
+          get: _ => v,
+          set: newV => {
+            v = newV;
+            this.#isUpdated.add(new ViewmodelValue(this.subkey, '', k, v));
+          },
+        });
+      }
+    });
+  }
+};
